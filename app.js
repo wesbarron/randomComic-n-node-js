5c04bdc5ffd6b2ce43b3f22f13250d8349beb3a8
wesbarron <barron_wesley@columbusstate.edu>
2019-10-21 20:23:17 +0000

    added api call

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/', function(req, res){
 
     var req = http.request(options, callback);
     req.end();
-    res.send("api response is: " + api_response);
+    res.send("api response is: " + req);
 });
 
 
