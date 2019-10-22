d46e0ed6daa732101fe9b656269acabc03b1ff3b
wesbarron <barron_wesley@columbusstate.edu>
2019-10-22 20:16:15 +0000

    ajax

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ request(url, (error, response, body)=> {
     res.redirect('/');
     });
 });
-*/
+
 
 app.post('/randomComic', function(req, res){
 
@@ -75,7 +75,7 @@ app.post('/randomComic', function(req, res){
     res.redirect('/');
 
 });
-
+*/
 
 http.createServer(app).listen(port, function(){
 
