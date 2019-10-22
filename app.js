8cb34a0b292beee55ddcf36c1914d3cd87412319
wesbarron <barron_wesley@columbusstate.edu>
2019-10-22 05:02:28 +0000

    request https

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ request(url, (error, response, body)=> {
     res.send(
         '<h2 style="text-align:center">' + comicResponse.title + '</h2>' +
         '<br>' +
-        '<img src="' + comicImg + '" width="55%" height="45%" style="text-align:center">');
+        '<img src="' + comicImg + '" width="55%" height="75%" style="align-self:center">');
 
     res.redirect('/');
     });
