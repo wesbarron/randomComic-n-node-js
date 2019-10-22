6fe37857b5191966e6114cdf1b32bc411b8654ad
wesbarron <barron_wesley@columbusstate.edu>
2019-10-22 03:47:37 +0000

    request https

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ request(url, (error, response, body)=> {
 
     var comicResponse = JSON.parse(body);
     comicImg = comicResponse.img;
-    res.append(comicImg);
+    res.send(<img src="<% comicImg %>">);
 
     res.redirect('/');
     });
