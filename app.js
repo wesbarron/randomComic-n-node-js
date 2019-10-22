a66444d4cc5fb84b6d00333f8c1dc6bc6183479b
wesbarron <barron_wesley@columbusstate.edu>
2019-10-22 02:17:48 +0000

    request https

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.get('/', function(req, res){
     }
 
     var req = https.request(options, callback);
-    var originalJSON = JSON.parse(req);
+    var originalJSON = data;
     res.send(originalJSON);
     req.end();
 
